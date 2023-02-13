bfb6f873670286dc104f691d1412f2ae0a14a21e
huangyuchao <87007@topscore.com.cn>
2023-02-14 00:49:08 +0800

    i
    i

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  
+  server:{
+    host:'0.0.0.0',//解决vite use--host to expose
+    port:8080,//配置端口
+    open:true,//配置默认打开浏览器
+  },
+  plugins: [react()],
 })
